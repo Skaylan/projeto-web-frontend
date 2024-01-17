1d418f654b0ff65dcbf03af73b9cc63642c4bac1
wilyJ80 <abjurandam@gmail.com>
2024-01-17 17:53:04 -0300

    Add defaults to main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
 <h1>Bem-vindos ao projeto web</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style>
+	* {
+		box-sizing: border-box;
+	}
+
+	:global(body) {
+		margin: 0;
+		padding: 0;
+	}
+</style>
