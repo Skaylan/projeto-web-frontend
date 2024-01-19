0e9f4e903191f6ef1eaa75d0e6bf8a28d615be63
Gustavo <gustavodiniz_contato@hotmail.com>
2024-01-19 18:25:47 -0300

    Style:header da aplicação

---
+++ src/lib/Header.svelte
@@ -0,0 +1,73 @@
+<script>
+    import logo from "../lib/assets/logo.png"
+    import perfil from "../lib/assets/Profile.png"
+    
+    </script>
+    
+    
+        
+        <header>
+            
+            <div class="logo"><a href="#"><img src="{logo}"></a></div>
+            
+            <nav>
+                <ul class="nav_links">
+                    <li> <a href="#">Inicio</a></li>
+                    <li> <a href="#">Filmes</a></li>
+                    <li> <a href="#">Sobre</a></li>
+                    <li> <a href="#">API</a></li>
+                </ul>
+            </nav>
+            <div class="perfil">
+                <a href="#"><img src="{perfil}"></a>
+            </div>
+            
+        </header>
+    
+    
+    
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600&display=swap');
+    
+    * {
+    
+    font-family: 'Inter', sans-serif;
+    font-weight: 500;
+    font-size: 30px;
+    color: white;
+    list-style: none;
+    text-decoration: none;
+    }
+    
+    .logo {
+    
+    width: 60%;
+    cursor: pointer;
+    }
+    
+    header {
+        /* background-color: black; */
+        display: flex;
+        justify-content: center;
+        align-items: center;
+       
+    }
+    
+    .nav_links {
+        display: flex;
+        padding: 50px;
+    }
+    
+    li a {
+        display: flex;
+        justify-content: space-between;
+        padding: 50px;
+        margin: 0;
+    
+    }
+    .perfil {
+        border: 1px 
+        
+    }
+    
+    </style>
