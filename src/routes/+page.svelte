7c07f900a92340015d4286440cfdbec309a133ec
Alysson <alyssonoliveira456@gmail.com>
2024-01-21 16:22:11 -0300

    style: Seção de Filmes Página Principal

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,46 @@
-<h1>Bem-vindos ao projeto web</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<head>
+	<link rel="stylesheet" type="text/css" href="src\estilo.css" />
+</head>
+<body>
+	<h1>Filmes</h1>
+	<div class="conteudo">
+
+		<div class="conteudo__imagem"><img src="src\imagens\ProjetoWeb1.jpg" alt="Imagem" />
+			<div class="conteudo__imagem__texto">
+				<h1 class="titulo__imagem">Titulo</h1>
+				<p class="texto__imagem">Descricao</p></div>
+		</div>
+		<div class="conteudo__imagem">
+			<img src="src\imagens\Divertidamente.jpg" alt="Imagem" />
+			<div class="conteudo__imagem__texto">
+				<h1 class="titulo__imagem">Titulo</h1>
+				<p class="texto__imagem">Descricao</p></div>
+		</div>
+
+		<div class="conteudo__imagem">
+			<img src="src\imagens\ProjetoWeb1.jpg" alt="Imagem 3" />
+			<div class="conteudo__imagem__texto">
+				<h1 class="titulo__imagem">Titulo</h1>
+				<p class="texto__imagem">Descricao</p></div>
+		</div>
+
+		<div class="conteudo__imagem"><img src="src\imagens\Ponyo.jpg" alt="Imagem 4" />
+			<div class="conteudo__imagem__texto">
+				<h1 class="titulo__imagem">Titulo</h1>
+				<p class="texto__imagem">Descricao</p></div>
+		</div>
+
+		<div class="conteudo__imagem">
+			<img src="src\imagens\FromUpOnPoppyHill.jpg" alt="Imagem 5" />
+			<div class="conteudo__imagem__texto">
+				<h1 class="titulo__imagem">Titulo</h1>
+				<p class="texto__imagem">Descricao</p></div>
+		</div>
+		<div class="conteudo__imagem"><img src="src\imagens\ProjetoWeb1.jpg" alt="Imagem 6" />
+			<div class="conteudo__imagem__texto">
+				<h1 class="titulo__imagem">Titulo</h1>
+				<p class="texto__imagem">Descricao</p></div>
+		</div>
+		
+	</div>
+</body>
