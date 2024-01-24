3ece41d8b25c357b40a5db1562aec154770c5aad
Lucas Oliveira <lucas_7tv@hotmail.com>
2024-01-24 12:20:25 -0300

    feat: Adição e configuração da biblioteca skeletonUI no projeto

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,30 @@
+
+// @ts-check
+import { join } from 'path';
+
+// 1. Import the Skeleton plugin
+import { skeleton } from '@skeletonlabs/tw-plugin';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{svelte, html, js, ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	// 2. Opt for dark mode to be handled via the class method
+	darkMode: 'class',
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		// 3. Append the path to the Skeleton package
+		join(require.resolve(
+			'@skeletonlabs/skeleton'),
+			'../**/*.{html,js,svelte,ts}'
+		)
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [
+		// 4. Append the Skeleton plugin (after other plugins)
+		skeleton({
+      themes: {preset: ["skeleton"]}
+    })
+	]
 }
-
+						
