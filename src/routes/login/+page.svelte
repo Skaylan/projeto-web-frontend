5b838b0dce30535b1e63eb39134da2b57f20fe1f
Carlos Erick <73728755+Carlos0101py@users.noreply.github.com>
2024-04-25 02:34:04 -0300

    Style: rota login convertida para tailwind

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -3,6 +3,7 @@
     import HeaderSecundario from "$lib/headerSecundario.svelte";
     import googleLogo from "$lib/assets/google-logo.svg";
     import Logo from "$lib/assets/Logo.png";
+    import mascote from "$lib/assets/mascote_login.svg";
     import Cookies from 'js-cookie'
     import { goto } from '$app/navigation'
     import { jwtDecode } from 'jwt-decode';
@@ -13,8 +14,7 @@
     let email;
     let password;
     let rePassword;
-
-    // $: console.log(name, username, email, password, rePassword)
+    let SizeField;
     
     const handleLoginButton = () => {
         const payload = {
@@ -48,13 +48,25 @@
 
 <HeaderSecundario  pagina="login" />
 
-<section class="flex flex-col h-lvh items-center justify-center">
-    <div class="flex-col w-[80%] h-[20rem] shadow-xl p-4">
-        <div class="flex justify-center items-center h-[5em]">
-            <h3 class="font-hanalei text-3xl">SEJA BEM-VINDO</h3>
+<div class="flex h-svh justify-center items-center mt-0">
+    <div class="h-[50%] w-[85%] flex  justify-center shadow-2xl lg:w-[50rem]">
+        <div class="hidden sm:bg-[#75C9C8] sm:flex w-full justify-center">
+
+            <div class="">
+                <img class="object-fill w-full h-full " src="{mascote}" alt="Descrição da imagem">
+            </div>
+
+            <div class="flex flex-col items-center justify-center">
+                <img class="w-[300px]" src="{Logo}" alt="">
+                <span class="font-hanalei text-sm">Seu acervo de histórias incríveis</span>
+            </div>
         </div>
 
-        <div class=" p-3">
+        <div class=" flex flex-col w-full justify-center lg:w-[50rem]">
+            <div class="flex justify-center items-center h-[5em]">
+                <h3 class="font-hanalei text-3xl">SEJA BEM-VINDO</h3>
+            </div>
+
             <div class="flex flex-col gap-2 items-center">
                 <div>
                     <input class="w-[15rem] h-[1.7rem] pl-1 rounded-md shadow-md" bind:value={email} type="email" name="email" id="iusuario" placeholder="Email" required>
@@ -62,10 +74,10 @@
                 <div>
                     <input class="w-[15rem] h-[1.7rem] rounded-md pl-1 shadow-md" bind:value={password} type="password" name="senha" id="isenha" min="8" max="20" placeholder="Senha" required>
                     <div class="flex justify-end pt-[4px]">
-                        <a class="text-[11px] hover:underline text-cyan-800 pr-2" href="#/" id="esqueci-senha">Esqueci minha senha</a>
+                        <a class="text-[11px] hover:underline hover:text-red-800 hover:font-bold text-cyan-800 pr-2" href="#/" id="esqueci-senha">Esqueci minha senha</a>
                     </div>
                 </div>
-            </div> 
+            </div>
 
             <div class="flex gap-3 items-center justify-center p-5">
                 <button class="h-[2rem] w-[5rem] rounded-md bg-green-500 transition transform hover:ease-in duration-300  hover:scale-110">
@@ -80,4 +92,4 @@
             </div>
         </div>
     </div>
-</section>
+</div>
