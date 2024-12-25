7c3697e6433c60ac03f729a81956776f36c145c7
Lucas Oliveira <lucas_7tv@hotmail.com>
2024-12-25 15:09:00 -0300

    Refactor: Refatorando o componente carrosel de filmes

--- src/lib/carrossel.svelte
+++ src/lib/carrossel.svelte
@@ -1,25 +1,40 @@
 <script>
-    import { Motion, useDragControls } from "svelte-motion";
-    const dragControls = useDragControls();
-    let divWidth = 0
-    let innerWidth = 0
-    let innerHeight = 0
+	import { Motion, useDragControls, useMotionValue, useVelocity } from 'svelte-motion';
+	import Device from 'svelte-device-info';
+	import MovieCard from './MovieCard.svelte';
+	const dragControls = useDragControls();
+	let divWidth = 0
+	let innerWidth = 0
+	let innerHeight = 0
+	let imageSize = 400;
+	export let movies;
+
 
 </script>
+
 <svelte:window bind:innerWidth bind:innerHeight />
-<div class="overflow-hidden">
-    <Motion drag="x" dragConstraints={{right: 0, left: -(3080-divWidth)}} let:motion {dragControls}>
-        <div bind:clientWidth={divWidth} use:motion class="flex gap-2 cursor-pointer w-[auto]" >
-            <img class="w-[300px] hover:w-[340px] h-[440px] transition-all" src="https://placehold.co/300x400" alt="movie poster" draggable="false">
-            <img class="w-[300px] hover:w-[340px] h-[440px] transition-all" src="https://placehold.co/300x400" alt="movie poster" draggable="false">
-            <img class="w-[300px] hover:w-[340px] h-[440px] transition-all" src="https://placehold.co/300x400" alt="movie poster" draggable="false">
-            <img class="w-[300px] hover:w-[340px] h-[440px] transition-all" src="https://placehold.co/300x400" alt="movie poster" draggable="false">
-            <img class="w-[300px] hover:w-[340px] h-[440px] transition-all" src="https://placehold.co/300x400" alt="movie poster" draggable="false">
-            <img class="w-[300px] hover:w-[340px] h-[440px] transition-all" src="https://placehold.co/300x400" alt="movie poster" draggable="false">
-            <img class="w-[300px] hover:w-[340px] h-[440px] transition-all" src="https://placehold.co/300x400" alt="movie poster" draggable="false">
-            <img class="w-[300px] hover:w-[340px] h-[440px] transition-all" src="https://placehold.co/300x400" alt="movie poster" draggable="false">
-            <img class="w-[300px] hover:w-[340px] h-[440px] transition-all" src="https://placehold.co/300x400" alt="movie poster" draggable="false">
-            <img class="w-[300px] hover:w-[340px] h-[440px] transition-all" src="https://placehold.co/300x400" alt="movie poster" draggable="false">
-        </div>
-    </Motion>
-</div>
+
+{#if innerWidth <= 640}
+	<div class="flex items-center text-4xl gap-1 sm:pl-14 p-4 sm:text-5xl">
+		<ul class="flex gap-4 snap-x overflow-x-auto snap-mandatory">
+			{#each movies as movie}
+				<li class="shrink-0 snap-center">
+					<MovieCard movie={movie} />
+				</li>
+			{/each}
+		</ul>
+	</div>
+
+{:else}
+	<Motion drag="x" let:motion {dragControls} dragConstraints={{ right: 0, left: -((movies.length * imageSize) - divWidth)}}>
+		<div bind:clientWidth={divWidth} class="flex items-center overflow-hidden md:pl-14">
+			<ul use:motion class="flex gap-4 snap-x snap-mandatory">
+				{#each movies as movie}
+					<li class="shrink-0 snap-center">
+						<MovieCard movie={movie}/>
+					</li>
+				{/each}
+			</ul>
+		</div>
+	</Motion>
+{/if}
