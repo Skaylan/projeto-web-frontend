e2fb4a8d5564f0170e137b69c4bba0537eac5684
Carlos Erick <73728755+Carlos0101py@users.noreply.github.com>
2025-01-10 01:47:32 -0300

    style: alteração no layout do componente de formulario

--- src/lib/movieForm.svelte
+++ src/lib/movieForm.svelte
@@ -22,7 +22,7 @@
 	let data = [];
 	export let activeCategories = [];
 
-	console.log(activeCategories)
+	console.log(activeCategories);
 
 	function handleImageChange(event, field) {
 		const selectedFile = event.target.files[0];
@@ -30,10 +30,8 @@
 			convertImageToBase64(selectedFile, (base64String) => {
 				if (field === 'imageOne') {
 					imageOneBase64 = base64String;
-					console.log('Imagem 1 (Base64):', imageOneBase64);
 				} else if (field === 'imageTwo') {
 					imageTwoBase64 = base64String;
-					console.log('Imagem 2 (Base64):', imageTwoBase64);
 				}
 			});
 		} else {
@@ -50,9 +48,8 @@
 			}
 		} else {
 			// Remove o ID da array de categorias ativas
-			activeCategories = activeCategories.filter(id => id !== categoryId);
+			activeCategories = activeCategories.filter((id) => id !== categoryId);
 		}
-		console.log('Categorias ativas:', activeCategories);
 	}
 
 	onMount(async () => {
@@ -72,135 +69,189 @@
 	});
 </script>
 
+<form>
+	<div class="space-y-12">
+		<div class="border-b border-gray-900/10 pb-12">
+			<h2 class="text-base/7 font-semibold text-gray-900">Cadastro de filme</h2>
+			<div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+				<div class="sm:col-span-4">
+					<label for="titulo" class="block text-sm/6 font-medium text-gray-900">Titulo</label>
+					<div class="mt-2">
+						<div
+							class="flex items-center rounded-md bg-white pl-3 outline outline-1 -outline-offset-1 outline-gray-300 focus-within:outline focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600"
+						>
+							<div class="shrink-0 select-none text-base text-gray-500 sm:text-sm/6"></div>
+							<input
+								bind:value={title}
+								type="text"
+								name="titulo"
+								id="titulo"
+								class="block min-w-0 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6"
+								placeholder="Digite..."
+							/>
+						</div>
+					</div>
 
-<div class="flex flex-col gap-2 sm:flex-row sm:justify-center">
-	<!-- Formulário -->
-	<section class="sm:w-[60%] md:w-[95%]">
-		<div class="flex justify-center w-full">
-			<div class="flex flex-col items-center p-2 sm:w-[90%] bg-[#F2F2F2] rounded-lg">
-				<div class="w-full flex justify-start pl-5 text-2xl">
-					<span>Informações do filme</span>
-				</div>
-				<!-- Campos do formulário -->
-				<div class="w-[90%]">
-					<span>Título</span>
-					<div class="h-8">
-						<input
-							bind:value={title}
-							id="titulo"
-							class="w-full h-full rounded-lg shadow-custom"
-							type="text"
-						/>
+					<label for="Titulo original" class="block text-sm/6 font-medium text-gray-900"
+						>Titulo original</label
+					>
+					<div class="mt-2">
+						<div
+							class="flex items-center rounded-md bg-white pl-3 outline outline-1 -outline-offset-1 outline-gray-300 focus-within:outline focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600"
+						>
+							<div class="shrink-0 select-none text-base text-gray-500 sm:text-sm/6"></div>
+							<input
+								bind:value={original_title}
+								type="text"
+								name="Titulo original"
+								id="Titulo original"
+								class="block min-w-0 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6"
+								placeholder="Digite..."
+							/>
+						</div>
 					</div>
-				</div>
-				<div class="w-[90%]">
-					<span>Título original</span>
-					<div class="h-8">
-						<input
-							bind:value={original_title}
-							id="tituloOriginal"
-							class="w-full h-full rounded-lg shadow-custom"
-							type="text"
-						/>
+
+					<label for="Titulo romanizado" class="block text-sm/6 font-medium text-gray-900"
+						>Titulo romanizado</label
+					>
+					<div class="mt-2">
+						<div
+							class="flex items-center rounded-md bg-white pl-3 outline outline-1 -outline-offset-1 outline-gray-300 focus-within:outline focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600"
+						>
+							<div class="shrink-0 select-none text-base text-gray-500 sm:text-sm/6"></div>
+							<input
+								bind:value={romanized_original_title}
+								type="text"
+								name="Titulo romanizado"
+								id="Titulo romanizado"
+								class="block min-w-0 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6"
+								placeholder="Digite..."
+							/>
+						</div>
 					</div>
-				</div>
-				<div class="w-[90%]">
-					<span>título original romanizado</span>
-					<div class="h-16">
-						<input
-							bind:value={romanized_original_title}
-							id="descricao"
-							class="w-full h-full rounded-lg shadow-custom"
-							type="text"
-						/>
-					</div>
-				</div>
-				<div class="w-[90%]">
-					<span>Descrição</span>
-					<div class="h-16">
-						<input
+
+					<label for="Descrição" class="block text-sm/6 font-medium text-gray-900">Descrição</label>
+					<div class="mt-2">
+						<textarea
 							bind:value={description}
-							id="descricao"
-							class="w-full h-full rounded-lg shadow-custom"
-							type="text"
-						/>
+							name="Descrição"
+							id="Descrição"
+							rows="3"
+							class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+						></textarea>
 					</div>
-				</div>
-				<div class="w-[90%]">
-					<span>Diretor</span>
-					<div class="h-8">
-						<input
-							bind:value={director}
-							id="diretor"
-							class="w-full h-full rounded-lg shadow-custom"
-							type="text"
-						/>
-					</div>
-				</div>
-				<div class="w-[90%]">
-					<span>Estúdio</span>
-					<div class="h-8">
-						<input
-							bind:value={studio}
-							id="estudio"
-							class="w-full h-full rounded-lg shadow-custom"
-							type="text"
-						/>
-					</div>
-				</div>
-				<div class="w-[90%]">
-					<span>Produtora</span>
-					<div class="h-8">
-						<input
-							bind:value={producer}
-							id="produtora"
-							class="w-full h-full rounded-lg shadow-custom"
-							type="text"
-						/>
-					</div>
-				</div>
-				<div class="w-[90%]">
-					<span>Nota</span>
-					<div class="h-8">
-						<input
-							bind:value={rating}
-							id="nota"
-							class="w-full h-full rounded-lg shadow-custom"
-							type="text"
-						/>
-					</div>
-				</div>
-				<div class="w-[90%]">
-					<span>Data de lançamento</span>
-					<div class="h-8">
-						<input
-							bind:value={lounch_date}
-							id="nota"
-							class="w-full h-full rounded-lg shadow-custom"
-							type="date"
-						/>
-					</div>
-				</div>
-				<div class="w-[90%]">
-					<span>Tempo</span>
-					<div class="h-8">
-						<input
-							bind:value={running_time}
-							id="tempo"
-							class="w-full h-full rounded-lg shadow-custom"
-							type="text"
-						/>
-					</div>
-				</div>
 
-				<!-- Gênero -->
-				<div class="flex items-center flex-col w-[90%] lg:flex-row-reverse">
-					<div class="w-[90%]">
-						<span>Gênero</span>
-						<div class="flex items-center flex-col p-2 bg-[#75C9C8] rounded-md">
+					<label for="Estudio" class="block text-sm/6 font-medium text-gray-900">Estudio</label>
+					<div class="mt-2">
+						<div
+							class="flex items-center rounded-md bg-white pl-3 outline outline-1 -outline-offset-1 outline-gray-300 focus-within:outline focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600"
+						>
+							<div class="shrink-0 select-none text-base text-gray-500 sm:text-sm/6"></div>
+							<input
+								bind:value={studio}
+								type="text"
+								name="Estudio"
+								id="Estudio"
+								class="block min-w-0 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6"
+								placeholder="Digite..."
+							/>
+						</div>
+					</div>
+
+					<label for="Diretor" class="block text-sm/6 font-medium text-gray-900">Diretor</label>
+					<div class="mt-2">
+						<div
+							class="flex items-center rounded-md bg-white pl-3 outline outline-1 -outline-offset-1 outline-gray-300 focus-within:outline focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600"
+						>
+							<div class="shrink-0 select-none text-base text-gray-500 sm:text-sm/6"></div>
+							<input
+								bind:value={director}
+								type="text"
+								name="Diretor"
+								id="Diretor"
+								class="block min-w-0 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6"
+								placeholder="Digite..."
+							/>
+						</div>
+					</div>
+
+					<label for="Produtor" class="block text-sm/6 font-medium text-gray-900">Produtor</label>
+					<div class="mt-2">
+						<div
+							class="flex items-center rounded-md bg-white pl-3 outline outline-1 -outline-offset-1 outline-gray-300 focus-within:outline focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600"
+						>
+							<div class="shrink-0 select-none text-base text-gray-500 sm:text-sm/6"></div>
+							<input
+								bind:value={producer}
+								type="text"
+								name="Produtor"
+								id="Produtor"
+								class="block min-w-0 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6"
+								placeholder="Digite..."
+							/>
+						</div>
+					</div>
+
+					<label for="Nota" class="block text-sm/6 font-medium text-gray-900">Nota</label>
+					<div class="mt-2">
+						<div
+							class="flex items-center rounded-md bg-white pl-3 outline outline-1 -outline-offset-1 outline-gray-300 focus-within:outline focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600"
+						>
+							<div class="shrink-0 select-none text-base text-gray-500 sm:text-sm/6"></div>
+							<input
+								bind:value={rating}
+								type="text"
+								name="Nota"
+								id="Nota"
+								class="block min-w-0 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6"
+								placeholder="Digite..."
+							/>
+						</div>
+					</div>
+
+					<label for="Data de lançamento" class="block text-sm/6 font-medium text-gray-900"
+						>Data de lançamento</label
+					>
+					<div class="mt-2">
+						<div
+							class="flex items-center rounded-md bg-white pl-3 outline outline-1 -outline-offset-1 outline-gray-300 focus-within:outline focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600"
+						>
+							<div class="shrink-0 select-none text-base text-gray-500 sm:text-sm/6"></div>
+							<input
+								bind:value={lounch_date}
+								type="date"
+								name="Data de lançamento"
+								id="Data de lançamento"
+								class="block min-w-0 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6"
+							/>
+						</div>
+					</div>
+
+					<label for="Minutagem" class="block text-sm/6 font-medium text-gray-900">Minutagem</label>
+					<div class="mt-2">
+						<div
+							class="flex items-center rounded-md bg-white pl-3 outline outline-1 -outline-offset-1 outline-gray-300 focus-within:outline focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600"
+						>
+							<div class="shrink-0 select-none text-base text-gray-500 sm:text-sm/6"></div>
+							<input
+								bind:value={running_time}
+								type="text"
+								name="Minutagem"
+								id="Minutagem"
+								class="block min-w-0 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6"
+								placeholder="Digite..."
+							/>
+						</div>
+					</div>
+
+					<div class="md:w-[44%]">
+						<label for="Minutagem" class="block text-sm/6 font-medium text-gray-900">Gênero</label>
+						<div
+							class="flex items-center flex-col p-2 outline outline-1 -outline-offset-1 outline-gray-300 rounded-md"
+						>
 							<div class="flex items-center w-full h-8 m-2">
 								<input
-									class="w-full pl-3 h-full rounded-lg shadow-custom bg-[#F2F2F2]"
+									class="w-full pl-3 h-full rounded-lg bg-[#F2F2F2]"
 									type="search"
 									placeholder="Buscar..."
 								/>
@@ -210,7 +261,9 @@
 							<div class="overflow-y-scroll max-h-40 w-full">
 								{#if data.length > 0}
 									{#each data as category, index}
-										<div class="flex justify-between items-center w-full px-2 py-2 bg-white rounded-md mb-2">
+										<div
+											class="flex justify-between items-center w-full px-2 py-2 bg-white rounded-md mb-2"
+										>
 											<label for="category-{index}">{category.name}</label>
 											<input
 												type="checkbox"
@@ -224,67 +277,82 @@
 									<p>Loading categories...</p>
 								{/if}
 							</div>
-							
 						</div>
 					</div>
-					<!-- Upload -->
-					<div class="w-full h-full">
-						<section class="flex gap-3 items-center h-full lg:flex-row p-2">
-							<!-- Upload para a primeira imagem -->
-							<div class="flex justify-center w-[90%] relative lg:h-70">
-								<label
-									class="flex justify-center items-center px-4 py-2 bg-blue-500 text-white rounded cursor-pointer w-full h-full"
-									for="imageOne"
-								>
-									Upload Imagem 1
-									<input
-										id="imageOne"
-										class="absolute top-0 left-0 w-full h-full bg-white opacity-0 cursor-pointer"
-										type="file"
-										on:change={(event) => handleImageChange(event, 'imageOne')}
-									/>
-									<img class="w-5 h-5" src={pageIcon} alt="Ícone da imagem 1" />
-								</label>
-								{#if imageOneBase64}
-									<div class="mt-2">
-										<img
-											src={imageOneBase64}
-											alt="Imagem 1"
-											class="w-[50px] h-[50px] object-cover"
-										/>
-									</div>
-								{/if}
-							</div>
 
-							<!-- Upload para a segunda imagem -->
-							<div class="flex justify-center w-[90%] relative lg:h-70">
-								<label
-									class="flex justify-center items-center px-4 py-2 bg-green-500 text-white rounded cursor-pointer w-full h-full"
-									for="imageTwo"
+					<label for="cover-photo" class="block text-sm/6 font-medium text-gray-900"
+						>Imagem do filme</label
+					>
+
+					<div class="flex flex-row gap-4 justify-center md:justify-start">
+						<div
+							class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10"
+						>
+							<div class="text-center">
+								<svg
+									class="mx-auto size-12 text-gray-300"
+									viewBox="0 0 24 24"
+									fill="currentColor"
+									aria-hidden="true"
+									data-slot="icon"
 								>
-									Upload Imagem 2
-									<input
-										id="imageTwo"
-										class="absolute top-0 left-0 w-full h-full bg-white opacity-0 cursor-pointer"
-										type="file"
-										on:change={(event) => handleImageChange(event, 'imageTwo')}
+									<path
+										fill-rule="evenodd"
+										d="M1.5 6a2.25 2.25 0 0 1 2.25-2.25h16.5A2.25 2.25 0 0 1 22.5 6v12a2.25 2.25 0 0 1-2.25 2.25H3.75A2.25 2.25 0 0 1 1.5 18V6ZM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0 0 21 18v-1.94l-2.69-2.689a1.5 1.5 0 0 0-2.12 0l-.88.879.97.97a.75.75 0 1 1-1.06 1.06l-5.16-5.159a1.5 1.5 0 0 0-2.12 0L3 16.061Zm10.125-7.81a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Z"
+										clip-rule="evenodd"
 									/>
-									<img class="w-5 h-5" src={pageIcon} alt="Ícone da imagem 2" />
-								</label>
-								{#if imageTwoBase64}
-									<div class="mt-2">
-										<img
-											src={imageTwoBase64}
-											alt="Imagem 2"
-											class="w-[50px] h-[50px] object-cover"
+								</svg>
+								<div class="mt-4 flex text-sm/6 text-gray-600">
+									<label
+										for="imageOne"
+										class="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+									>
+										<span>Upload a Poster</span>
+										<input 
+										id="imageOne" name="imageOne" type="file" class="sr-only"
+										on:change={(event) => handleImageChange(event, 'imageOne')}
 										/>
-									</div>
-								{/if}
+									</label>
+								</div>
+								<p class="text-xs/5 text-gray-600">PNG, JPG</p>
 							</div>
-						</section>
+						</div>
+
+						<div
+							class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10"
+						>
+							<div class="text-center">
+								<svg
+									class="mx-auto size-12 text-gray-300"
+									viewBox="0 0 24 24"
+									fill="currentColor"
+									aria-hidden="true"
+									data-slot="icon"
+								>
+									<path
+										fill-rule="evenodd"
+										d="M1.5 6a2.25 2.25 0 0 1 2.25-2.25h16.5A2.25 2.25 0 0 1 22.5 6v12a2.25 2.25 0 0 1-2.25 2.25H3.75A2.25 2.25 0 0 1 1.5 18V6ZM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0 0 21 18v-1.94l-2.69-2.689a1.5 1.5 0 0 0-2.12 0l-.88.879.97.97a.75.75 0 1 1-1.06 1.06l-5.16-5.159a1.5 1.5 0 0 0-2.12 0L3 16.061Zm10.125-7.81a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Z"
+										clip-rule="evenodd"
+									/>
+								</svg>
+								<div class="mt-4 flex text-sm/6 text-gray-600">
+									<label
+										for="imageTwo"
+										class="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+									>
+										<span>Upload a Banner</span>
+										<input
+										id="imageTwo" name="imageTwo" type="file" class="sr-only"
+										on:change={(event) => handleImageChange(event, 'imageTwo')}
+										/>
+									</label>
+								</div>
+								<p class="text-xs/5 text-gray-600">PNG, JPG</p>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-	</section>
-</div>
+	</div>
+</form>
